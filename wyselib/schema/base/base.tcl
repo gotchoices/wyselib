7d6b86f218372847e9f3b283cc308e7d520da4cf
kylebateman <kylebateman@16d2b9bb-ca53-4060-9774-6f69b3e42227>
2017-03-27 20:19:56 +0000

    Improve column naming

--- wyselib/schema/base/base.tcl
+++ wyselib/schema/base/base.tcl
@@ -4,13 +4,13 @@ require ../common.tcl ../audit.tcl ../glob.tcl
 
 namespace eval base {
     def ent_pk          {id}
-    def ent_v_in        {ent_name ent_type ent_cmt fir_name mid_name pref_name title gender marital born_date username database activ inside country tax_id bank proxy}
+    def ent_v_in        {ent_name ent_type ent_cmt fir_name mid_name pref_name title gender marital born_date username database ent_inact inside country tax_id bank proxy}
     def ent_v_up        "$ent_v_in"
     def ent_se          [concat $ent_pk $ent_v_in $glob::stampfn]
     def ent_v_se        [concat $ent_se std_name frm_name cas_name]
     def ent_local       {}
     def ent_audit       "$ent_v_in"
-    def ent_v_pub_se    [concat id std_name ent_type username activ inside $glob::stampfn]
+    def ent_v_pub_se    [concat id std_name ent_type username ent_inact inside $glob::stampfn]
     def ent_entmin      {100}
     def ent_insmin      {1000}
     def ent_outmin      {10000}
