65a35b7760f2e1e7ab33be6553724e1a1eae2d58
kylebateman <kylebateman@16d2b9bb-ca53-4060-9774-6f69b3e42227>
2017-04-01 23:33:17 +0000

    Better logic for ID number ranges

--- wyselib/schema/base/base.tcl
+++ wyselib/schema/base/base.tcl
@@ -7,13 +7,14 @@ namespace eval base {
     def ent_v_in        {ent_name ent_type ent_cmt fir_name mid_name pref_name title gender marital born_date username database ent_inact inside country tax_id bank proxy}
     def ent_v_up        "$ent_v_in"
     def ent_se          [concat $ent_pk $ent_v_in $glob::stampfn]
-    def ent_v_se        [concat $ent_se std_name frm_name cas_name]
+    def ent_v_se        [concat $ent_se std_name frm_name giv_name cas_name]
     def ent_local       {}
     def ent_audit       "$ent_v_in"
     def ent_v_pub_se    [concat id std_name ent_type username ent_inact inside $glob::stampfn]
-    def ent_entmin      {100}
-    def ent_insmin      {1000}
-    def ent_outmin      {10000}
+    def ent_rolmin      {1}
+    def ent_orgmin      {100}
+    def ent_grpmin      {1000}
+    def ent_permin      {10000}
     def ent_link_pk     {org mem}
     def ent_link_v_in   [concat $ent_link_pk role]
     def ent_link_v_up   {role}
