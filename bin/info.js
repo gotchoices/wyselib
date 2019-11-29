3cef4a205f69270c25da156e4b91dd2c9e06f21f
Kyle Bateman <kyle@lux0.batemans.org>
2019-11-29 14:12:00 -0700

    Add path option to information script

--- bin/info.js
+++ bin/info.js
@@ -1,9 +1,18 @@
-#!/usr/bin/env node
+#!/bin/node
 //Return information about the currently installed wylib package
 //Copyright WyattERP.org; See license in root of this package
 // -----------------------------------------------------------------------------
 const Path = require("path")
 var package = require('../package.json')
+var self = Path.normalize(Path.join(__dirname, '..'))
 var parent = Path.normalize(Path.join(__dirname, '../..'))
 
-console.log(parent, package.name, package.version)
+//console.log('argv:', process.argv)
+if (process.argv[2] == 'path')
+  console.log(self)
+else if (process.argv[2] == 'name')
+  console.log(package.name)
+else if (process.argv[2] == 'version')
+  console.log(package.version)
+else
+  console.log(parent, package.name, package.version)
